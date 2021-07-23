52a699c0d6c23c0e79bf0a3a591946001611de2a
edwardCwh11039 <51016142+edwardCwh11039@users.noreply.github.com>
2021-07-23 17:56:11 +1000

    home page

---
+++ components/normal-item.js
@@ -0,0 +1,66 @@
+import { Col } from "antd";
+import styled from "styled-components";
+
+export const Container = styled.div`
+  padding-right: 15px;
+  padding-left: 15px;
+  margin-right: auto;
+  margin-left: auto;
+
+  @media (max-width: 1200px) {
+    width: 1170px;
+  }
+  @media (max-width: 992px) {
+    width: 970px;
+  }
+  @media (max-width: 768px) {
+    width: 750px;
+  }
+`;
+
+export const BannerH1 = styled.h1`
+  color: #fff;
+  margin-bottom: 0;
+  @media (min-width: 1200px) {
+    font-size: 46px;
+  }
+  @media (min-width: 993px) and (max-width: 1199px) {
+    font-size: 46px;
+  }
+  @media (min-width: 768px) and (max-width: 992px) {
+    font-size: 46px;
+  }
+  @media (max-width: 767px) {
+    font-size: 28px;
+  }
+`;
+
+export const BannerH2 = styled.h2`
+  color: #fff;
+  margin-bottom: 0;
+  @media (min-width: 1200px) {
+    font-size: 90px;
+  }
+  @media (min-width: 993px) and (max-width: 1199px) {
+    font-size: 75px;
+  }
+  @media (min-width: 768px) and (max-width: 992px) {
+    font-size: 64px;
+  }
+  @media (max-width: 767px) {
+    font-size: 38px;
+  }
+`;
+
+export default function ServiceCol({ img, ...props }) {
+  return (
+    <Col span={12}>
+      <a href="">
+        <div className="service-boxs">
+          <img src={img} style={{ verticalAlign: "middle", border: 0 }}></img>
+          {props.children}
+        </div>
+      </a>
+    </Col>
+  );
+}
